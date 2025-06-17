0d2cc287fd138674c23f1ddf42517de0526354fb
Usman Shittu <usmanola1995@gmail.com>
2025-06-18 00:54:34 +0100

    implementing local storage for tod0-list

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ document.addEventListener("DOMContentLoaded", function(){
     const addButton = document.getElementById("add-task-btn");
     const taskInput = document.getElementById("task-input");
     const taskList = document.getElementById("task-list");
+
+    loadTasks();
+if (save) {
+        const storedTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+        storedTasks.push(taskText);
+        localStorage.setItem('tasks', JSON.stringify(storedTasks));
+    }
+    
+    function loadTasks() {
+    const storedTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+    storedTasks.forEach(taskText => addTask(taskText, false)); // 'false' indicates not to save again to Local Storage
+}
     function addTask() {
         const taskText = taskInput.value.trim();
         if (taskText === "") {
